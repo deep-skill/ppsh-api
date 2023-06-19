6eb8a83b50e77ec8f6c86adf9c3647d0cdcefb76
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-19 18:13:18 -0300

    connection with mySQL DB done.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
 const server = require('./src/app');
+const { conn } =require('./src/db');
 
+// ###### FUNCTION TO TEST CONNECTION WITH DATABASE ###### 
+// const testdb = async () => {try {
+//     await conn.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
+// testdb()
 
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-    console.log(`Server in port ${PORT}`);
-});
+
+conn.sync({ force: false }).then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server in port ${PORT}`);
+    });
+})
+.catch((error) => console.log(error));
--- src/db.js
+++ src/db.js
@@ -1,1 +1,19 @@
-// conexion a DB
+// DB conexion
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const {
+    DB, DB_USERNAME, DB_PASSWORD, DB_HOST
+} = process.env;
+
+
+// Database Instance
+const sequelize = new Sequelize(DB, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'mysql'
+});
+
+
+
+module.exports = {
+    conn: sequelize
+}
