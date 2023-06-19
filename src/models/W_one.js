a8030f1d0dd409b8d4be0c602e32f66951f6f34c
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-19 19:53:26 -0300

    models DB / defining model w_one

---
+++ src/models/W_one.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "w_one",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Y_y: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+      Y_z: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+      Y_mc: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+      Y_ab: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+      Y_bc: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
