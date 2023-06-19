42426b11a0b807e7dec07508b86f8ec9d536ba7c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-19 20:56:22 -0300

    Point_polygon model defined.

---
+++ src/models/PolygonPoints.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+// Export model as a function
+module.exports = (sequelize) => {
+    sequelize.define('PolygonPoints', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        latitude: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        longitude: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+        {
+            timestamps: false
+        });
+};
