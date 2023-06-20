85f6111405d52c2f265c7477f1081815ef372980
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-20 14:27:30 -0300

    Controller 'getUniformHazardSpectrum' Done.

---
+++ src/controllers/getUniformHazardSpectrum.js
@@ -0,0 +1,14 @@
+const getUniformHazardSpectrum = (location, tr) => {
+    const result = [];
+    const hazardValues = hazard(location, tr)  
+
+    for(let key in hazardValues){
+        if(hazardValues.hasOwnProperty(key)){
+            result.push({ x: key, y:hazardValues[key]})
+        }
+    }
+
+    return result;
+};
+
+module.exports = getUniformHazardSpectrum;
