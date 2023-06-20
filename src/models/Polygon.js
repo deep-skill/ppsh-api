c900421fe6994d1e1af58fd8c09fa21096d5ec36
Facundo Pucheta <facundopucheta94@gmail.com>
2023-06-20 10:07:28 -0300

    feat(/src/models): new Polygon model added

---
+++ src/models/Polygon.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Polygon",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      points: {
+        type: DataTypes.STRING(2860),
+        defaultValue: null,
+      },
+      type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
