d19c89f78779cc70e7238e14f767f578e0198a5f
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-19 21:52:00 -0300

    models DB / defining models Zer0 - Zer3

---
+++ src/models/Zer2.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "zer2",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      period: {
+        type: DataTypes.STRING(5),
+        defaultValue: null,
+      },
+      X: {
+        type: DataTypes.STRING(10),
+        defaultValue: null,
+      },
+      Y_y: {
+        type: DataTypes.STRING(30),
+        defaultValue: null,
+      },
+      Y_z: {
+        type: DataTypes.STRING(30),
+        defaultValue: null,
+      },
+      Y_mc: {
+        type: DataTypes.STRING(30),
+        defaultValue: null,
+      },
+      Y_ab: {
+        type: DataTypes.STRING(30),
+        defaultValue: null,
+      },
+      Y_bc: {
+        type: DataTypes.STRING(30),
+        defaultValue: null,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
