56bef010b894c8e2401b587c1a7b572e58a6372e
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-21 17:43:52 -0300

    feature add / getPeriodArray code passed to a helper to refactor and reduce redundant code

---
+++ src/helpers/getPeriodArray.js
@@ -0,0 +1,15 @@
+const getPeriodArray = () => {
+  let period = [0.0, 0.05, 0.075];
+
+  for (let i = 0.1; i < 1.0; i += 0.05) {
+    period.push(i);
+  }
+  for (let i = 1.0; i < 3.1; i += 0.1) {
+    period.push(i);
+  }
+  return period;
+};
+
+module.exports = {
+  getPeriodArray,
+};
