49924772fef8e1068c05b4b546c2bcaed0d4297a
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-21 13:07:48 -0300

    feature add / getPolygon

---
+++ src/helpers/getPolygon.js
@@ -0,0 +1,15 @@
+const { isInsidePolygon } = requier("./isInsidePolygon");
+
+const getPolygono = (latitude, longitude, polygons, coordinates) => {
+  let polygon;
+
+  for (let i = 0; i < polygons.length; i++) {
+    polygon = isInsidePolygon(latitude, longitude, i, polygons, coordinates);
+    if (polygon) return i;
+  }
+  return -1;
+};
+
+module.exports = {
+  getPolygono,
+};
