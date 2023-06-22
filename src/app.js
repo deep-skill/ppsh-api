7f8b9d2c19163c41c276afeeccbd6e0403759c9a
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-22 14:02:10 -0300

    Server middlewares added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 // server root
-const express = require('express');
-const server = express();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index");
 
+const server = express();
 
 // middleswares
 server.use(express.json());
+server.use(cookieParser(morgan("dev")));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 //db
-//routes 
+require("./db");
 
-
+//routes
+server.use("/psp", routes);
 
 module.exports = server;
