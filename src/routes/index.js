3d2cf9cf03b0ae45a3ad0994a90736cd95ada5c3
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-23 16:58:47 -0300

    Enpoins defined

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const router = Router();
+const getDesignSpectrumHandler = require("../handlers/getDesignSpectrumHandler");
+const getExceedanceProbabilityHandler = require("../handlers/getExceedanceProbabilityHandler");
+const getLocationsHandler = require("../handlers/getLocationsHandler");
+const getUniformHazardSpectrumHandler = require("../handlers/getUniformHazardSpectrumHandler");
 
-router.get("/", (req, res) => {
-  res.send("Is working");
-});
+router.get("/designspectrum", getDesignSpectrumHandler);
+router.get("/eprobability", getExceedanceProbabilityHandler);
+router.get("/location", getLocationsHandler);
+router.get("/hazardspectrum", getUniformHazardSpectrumHandler);
 
 module.exports = router;
