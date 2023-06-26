ab57ca3b19c81cb2901c8df95877da5d11232c2c
Facundo Pucheta <facundopucheta94@gmail.com>
2023-06-26 16:32:48 -0300

    feat(/tests/routes): new tests added for /eprobability route

--- tests/routes/getLocationsHandler.spec.js
+++ tests/routes/getLocationsHandler.spec.js
@@ -25,27 +25,17 @@ describe(`Route GET '/location' status codes`, () => {
 
 describe(`Route GET '/location' responses`, () => {
   test("Response property for location: test 1", async () => {
-    const res = await request(app)
+    const res1 = await request(app)
       .get("/psp/location?lat=-12.1&long=-77")
       .send();
-    expect(res.body).toHaveProperty("status", "success");
-    expect(res.body).toHaveProperty("data");
-    expect(res.body).toHaveProperty("total");
-  });
-
-  test("Response property for location: test 2", async () => {
-    const res1 = await request(app)
-      .get("/psp/location?lat=-4.9&long=-74.8")
-      .send();
-
     expect(res1.body).toHaveProperty("status", "success");
     expect(res1.body).toHaveProperty("data");
     expect(res1.body).toHaveProperty("total");
   });
 
-  test("Response property for location: test 3", async () => {
+  test("Response property for location: test 2", async () => {
     const res2 = await request(app)
-      .get("/psp/location?lat=-16.3&long=-71.6")
+      .get("/psp/location?lat=-4.9&long=-74.8")
       .send();
 
     expect(res2.body).toHaveProperty("status", "success");
@@ -53,13 +43,23 @@ describe(`Route GET '/location' responses`, () => {
     expect(res2.body).toHaveProperty("total");
   });
 
-  test("Bad request respone: without data query", async () => {
+  test("Response property for location: test 3", async () => {
+    const res3 = await request(app)
+      .get("/psp/location?lat=-16.3&long=-71.6")
+      .send();
+
+    expect(res3.body).toHaveProperty("status", "success");
+    expect(res3.body).toHaveProperty("data");
+    expect(res3.body).toHaveProperty("total");
+  });
+
+  test("Bad request response: without data query", async () => {
     const res = await request(app).get("/psp/location").send();
 
     expect(res.body).toHaveProperty("message");
   });
 
-  test("Bad request respone: Data is not in database", async () => {
+  test("Bad request response: Data is not in database", async () => {
     const res = await request(app).get("/psp/location?lat=0&long=0").send();
 
     expect(res.body).toHaveProperty("message");
