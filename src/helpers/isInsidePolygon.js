70875cea35e746fd404b9a0605e1b256d4a63024
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-26 12:05:16 -0300

    fix error helpers / getDBCoordinates, getDBPolygon, probabilities, isInsidePolygon, getPolygon

--- src/helpers/isInsidePolygon.js
+++ src/helpers/isInsidePolygon.js
@@ -1,25 +1,25 @@
-const isInsidePolygon = async (lat, long, k, polygons, coordinates) => {
-  
+const isInsidePolygon = (lat, long, k, polygons, coordinates) => {
+
   const nVert = polygons[k].length;
   const testY = lat;
   const testX = long;
 
-  let vertY;
-  let vertX;
+  let vertY = [];
+  let vertX = [];
 
   for (let t = 0; t < nVert; t++) {
-    vertY = coordinates[polygons[k][t] - 1][0];
-    vertX = coordinates[polygons[k][t] - 1][1];
+    vertY.push(coordinates[polygons[k][t] - 1][0]);
+    vertX.push(coordinates[polygons[k][t] - 1][1]);
   }
-
-  let c = 0;
-  const helper = vertY[i] > testY !== vertY[j] > testY && testX < ((vertX[j] - vertX[i]) * (testY - vertY[i])) / (vertY[j] - vertY[i]) + vertX[i];
 
   for (let i = 0, j = nVert - 1; i < nVert; j = i++) {
-    if (helper) c = !c;
+    if (((vertY[i] > testY) !== (vertY[j] > testY)) &&
+      (testX < (vertX[j] - vertX[i]) * (testY - vertY[i]) / (vertY[j] - vertY[i]) + vertX[i])) {
+      return true;
+    }
   }
 
-  return c;
+  return false;
 };
 
 module.exports = isInsidePolygon;
--- src/helpers/probabilities.js
+++ src/helpers/probabilities.js
@@ -1,157 +1,136 @@
-const { Polygon, PolygonPoints, Location, W_one, Zer0, Zer1, Zer2, Zer3, Zer4,
-    Zer5, Zer6, Zer7} = require("../db");
-const getPolygon = require("./getPolygon")    ;
+const { Location, W_one, Zer0, Zer1, Zer2, Zer3, Zer4,
+  Zer5, Zer6, Zer7 } = require("../db");
+const getPolygon = require("./getPolygon");
+const getDBCoordinates = require("./getDBCoordinates");
+const getDBPolygons = require("./getDBPolygons");
 
 
-const probabilities = async (location, period)=>{
+const probabilities = async (location, period) => {
 
-  let coordinatesData;
   let coordinates = [];
-  let polygonsData;
   let polygons = [];
   let zer_data;
   let sum;
   let result = [];
-	let X = [];
-	let Y_y = [];
-	let Y_z = [];
-	let Y_mc = [];
-	let Y_ab = [];
-	let Y_bc = [];
+  let X = [];
+  let Y_y = [];
+  let Y_z = [];
+  let Y_mc = [];
+  let Y_ab = [];
+  let Y_bc = [];
 
-    
+  try {
+    const locationData = await Location.findByPk(location);
 
-  const locationData = await Location.findByPk(location);
+    if (!locationData) throw new Error("Location not found");
 
-  if (!locationData) throw new Error("Location not found");
+    const latitude = locationData.latitude;
+    const longitude = locationData.longitude;
 
-  const latitude = locationData.latitude;
-  const longitude = locationData.longitude;
+    coordinates = await getDBCoordinates(1);
 
-  coordinatesData = await PolygonPoints.findAll({ where: { type: 1 } });
+    polygons = await getDBPolygons(1);
 
-  if (!coordinatesData) throw new Error("Coordinates not found");
+    const polygon = getPolygon(latitude, longitude, polygons, coordinates);
 
-  for (const coord of coordinatesData) {
-    coordinates.push(coord.latitude, coord.longitude);
+    if (polygon === -1) throw new Error("Polygon not found");
+
+    const ponderationsData = await W_one.findAll();
+
+    if (ponderationsData < 0) throw new Error("Ponderations not found");
+
+    let ponderations = [];
+
+    for (const pond of ponderationsData) {
+      ponderations.push([pond.Y_y, pond.Y_z, pond.Y_mc, pond.Y_ab, pond.Y_bc]);
+    }
+
+    coordinates = await getDBCoordinates(4);
+
+    polygons = await getDBPolygons(4);
+
+    const polygon2 = getPolygon(
+      latitude,
+      longitude,
+      polygons,
+      coordinates
+    );
+
+    if (polygon2 === -1) throw new Error("Polygon not found");
+
+    switch (polygon2) {
+      case 0:
+        zer_data = await Zer0.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 1:
+        zer_data = await Zer1.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 2:
+        zer_data = await Zer2.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 3:
+        zer_data = await Zer3.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 4:
+        zer_data = await Zer4.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 5:
+        zer_data = await Zer5.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 6:
+        zer_data = await Zer6.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      case 7:
+        zer_data = await Zer7.findAll({
+          where: { id: location, Periodo: period },
+        });
+        break;
+      default:
+        throw new Error("Polygon is out of parameters");
+    }
+
+    if (!zer_data) throw new Error("Information not found");
+
+    for (const data of zer_data) {
+      X.push(data.X);
+      Y_y.push(data.Y_y);
+      Y_z.push(data.Y_z);
+      Y_mc.push(data.Y_mc);
+      Y_ab.push(data.Y_ab);
+      Y_bc.push(data.Y_bc);
+    }
+
+
+    for (let i = 0; i < 20; i++) {
+      sum =
+        ponderations[polygon][0] * Y_y[i] +
+        ponderations[polygon][1] * Y_z[i] +
+        ponderations[polygon][2] * Y_mc[i] +
+        ponderations[polygon][3] * Y_ab[i] +
+        ponderations[polygon][4] * Y_bc[i];
+      sum = sum.toFixed(8);
+      result.push({ x: X[i] / 981, y: sum });
+    }
+
+    return result;
+
+  } catch (error) {
+    return error;
   }
-
-  polygonsData = await Polygon.findAll({ where: { type: 1 } });
-
-  if (!polygonsData) throw new Error("Polygons not found");
-
-  for (const pol of polygonsData) {
-    polygons.push(pol.point.split("|"));
-  }
-
-  const polygon = getPolygon(latitude, longitude, polygons, coordinates);
-
-  if (polygon === -1) throw new Error("Polygon not found");
-
-  const ponderationsData = W_one.findAll();
-  
-  let ponderations = [];
-
-  for (const pond of ponderationsData) {
-    ponderations.push(pond.Y_y, pond.Y_z, pond.Y_mc, pond.Y_ab, pond.Y_bc);
-  }
-
-  coordinatesData = await PolygonPoints.findAll({ where: { type: 4 } });
-  
-  if (!coordinatesData) throw new Error("Coordinates not found");
-
-  coordinates = [];
-  for (const coord of coordinatesData) {
-    coordinates.push(coord.latitude, coord.longitude);
-  }
-
-  polygonsData = await Polygon.findAll({ where: { type: 4 } });
-
-  if (!polygonsData) throw new Error("Polygons not found");
-
-  polygons = [];
-  for (const pol of polygonsData) {
-    polygons.push(pol.point.split("|"));
-  }
-
-  const polygon2 = await getPolygon(
-    latitude,
-    longitude,
-    polygons,
-    coordinates
-  );
-
-  if (polygon2 === -1) throw new Error("Polygon not found");
-
-  switch (polygon2) {
-    case 0:
-      zer_data = await Zer0.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 1:
-      zer_data = await Zer1.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 2:
-      zer_data = await Zer2.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 3:
-      zer_data = await Zer3.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 4:
-      zer_data = await Zer4.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 5:
-      zer_data = await Zer5.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 6:
-      zer_data = await Zer6.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    case 7:
-      zer_data = await Zer7.findAll({
-        where: { id: location, Periodo: period },
-      });
-      break;
-    default:
-      throw new Error("Polygon is out of parameters");
-  }
-
-  if (!zer_data) throw new Error("Information not found");
-
-  for (const data of zer_data) {
-    X.push(data.X);
-    Y_y.push(data.Y_y);
-    Y_z.push(data.Y_z);
-    Y_mc.push(data.Y_mc);
-    Y_ab.push(data.Y_ab);
-    Y_bc.push(data.Y_bc);
-  }
-
-  
-  for (let i = 0; i < 20; i++) {
-    sum =
-      ponderations[polygon][0] * Y_y[i] +
-      ponderations[polygon][1] * Y_z[i] +
-      ponderations[polygon][2] * Y_mc[i] +
-      ponderations[polygon][3] * Y_ab[i] +
-      ponderations[polygon][4] * Y_bc[i];
-    sum = sum.toFixed(8);
-    result.push({ x: X[i] / 981, y: sum });
-  }
-
-  return result;
 };
 
 module.exports = probabilities;
