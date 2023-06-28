2c5e40c7a61abbe953f987bedc4fcb25da9288c6
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-06-28 15:49:57 -0300

    fix helper / isInsidePolygos - change data type from string to number - change return variable for better response

--- src/helpers/isInsidePolygon.js
+++ src/helpers/isInsidePolygon.js
@@ -1,25 +1,29 @@
 const isInsidePolygon = (lat, long, k, polygons, coordinates) => {
 
   const nVert = polygons[k].length;
-  const testY = lat;
-  const testX = long;
+  const testY = +lat;
+  const testX = +long;
 
+  const numberCoordinates = coordinates.map(array => array.map(coordinate => +coordinate))
+  
   let vertY = [];
   let vertX = [];
 
   for (let t = 0; t < nVert; t++) {
-    vertY.push(coordinates[polygons[k][t] - 1][0]);
-    vertX.push(coordinates[polygons[k][t] - 1][1]);
+    vertY.push(numberCoordinates[polygons[k][t] - 1][0]);
+    vertX.push(numberCoordinates[polygons[k][t] - 1][1]);
   }
+
+  let response = 0;
 
   for (let i = 0, j = nVert - 1; i < nVert; j = i++) {
     if (((vertY[i] > testY) !== (vertY[j] > testY)) &&
       (testX < (vertX[j] - vertX[i]) * (testY - vertY[i]) / (vertY[j] - vertY[i]) + vertX[i])) {
-      return true;
+      response = !response;
     }
   }
 
-  return false;
+  return response;
 };
 
 module.exports = isInsidePolygon;
