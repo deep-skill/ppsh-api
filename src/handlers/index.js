c89fd5cee5610bf4760585d99b62647786b43f2d
Facundo Pucheta <facundopucheta94@gmail.com>
2023-06-30 12:51:19 -0300

    feat(/src): new 'standards' feature added
    - /src/routes : added new route & imports modularized
    - /src/handlers : added new handler
    - /src/controllers : added new controller

---
+++ src/handlers/index.js
@@ -0,0 +1,13 @@
+const getDesignSpectrumHandler = require("../handlers/getDesignSpectrumHandler");
+const getExceedanceProbabilityHandler = require("../handlers/getExceedanceProbabilityHandler");
+const getLocationsHandler = require("../handlers/getLocationsHandler");
+const getUniformHazardSpectrumHandler = require("../handlers/getUniformHazardSpectrumHandler");
+const getStandardsHandler = require("../handlers/getStandardsHandler");
+
+module.exports = {
+  getDesignSpectrumHandler,
+  getExceedanceProbabilityHandler,
+  getLocationsHandler,
+  getUniformHazardSpectrumHandler,
+  getStandardsHandler,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const getDesignSpectrumHandler = require("../handlers/getDesignSpectrumHandler");
-const getExceedanceProbabilityHandler = require("../handlers/getExceedanceProbabilityHandler");
-const getLocationsHandler = require("../handlers/getLocationsHandler");
-const getUniformHazardSpectrumHandler = require("../handlers/getUniformHazardSpectrumHandler");
+const {
+  getDesignSpectrumHandler,
+  getExceedanceProbabilityHandler,
+  getLocationsHandler,
+  getUniformHazardSpectrumHandler,
+  getStandardsHandler
+} = require("../handlers");
 
 router.get("/designspectrum", getDesignSpectrumHandler);
 router.get("/eprobability", getExceedanceProbabilityHandler);
 router.get("/location", getLocationsHandler);
 router.get("/hazardspectrum", getUniformHazardSpectrumHandler);
+router.get("/standards", getStandardsHandler);
 
 module.exports = router;
